12c53cb4b3ed9cd57156c89dc59f285a8d407f09
EC2 Default User <ec2-user@ip-172-16-184-54.ec2.internal>
2020-11-11 09:55:25 +0000

    add preprocess.py

---
+++ containers/scripts/preprocess/preprocess.py
@@ -0,0 +1,9 @@
+import argparse
+import os
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    #parser.add_argument("--train-test-split-ratio", type=float, default=0.3)
+    args, _ = parser.parse_known_args()
+    print('Do something')
